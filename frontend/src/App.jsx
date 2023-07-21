67afe002db480a84264e6e70a9f15ff8215dbe29
g-rima <gikandimaria178@gmail.com>
2023-07-21 11:08:47 +0300

    created api file to connect to backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import SignupForm from "./components/SignupForm"
 import LoginForm from "./components/LoginForm";
+import Home from "./components/Home";
 import { BrowserRouter, Routes , Route } from 'react-router-dom';
 
 export default function App() {
@@ -9,6 +10,7 @@ export default function App() {
             <Routes>
                 <Route path="/login" element={< LoginForm />}></Route>
                 <Route path="/signup" element={< SignupForm />}></Route>
+                <Route path="/Home" element={< Home />}></Route>
             </Routes>
         </BrowserRouter>
         );
